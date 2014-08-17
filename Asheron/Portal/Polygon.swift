9cf77cd743ad93a9c8eb8c33361a3bb5275a6683
jkolb <franticapparatus@gmail.com>
2014-08-16 23:52:34 -0500

    Update Asheron with new potential known data (hasBackFace, frontMaterialIndex, backMaterialIndex) 01000E61 (a type 3) seems to have these values but they don't match what is expected like in 01000F42. Added option to output mesh info to help with reverse engineering.

--- Asheron/Portal/Polygon.swift
+++ Asheron/Portal/Polygon.swift
@@ -13,7 +13,10 @@ extension ByteBuffer {
         let index = getIntFrom16Bits()
         let count = getIntFrom8Bits()
         let type = getIntFrom8Bits()
-        let unknown1 = getUInt16(4)
+        let hasBackFace = getUInt16()
+        let unknown1 = getUInt16()
+        let frontMaterialIndex = getIntFrom16Bits()
+        let backMaterialIndex = getIntFrom16Bits()
         let vertexIndex = getIntFrom16Bits(count)
         var texcoordIndex: Array<Int>
         var unknown2: Array<UInt8>
@@ -34,7 +37,10 @@ extension ByteBuffer {
             index: index,
             count: count,
             type: type,
+            hasBackFace: hasBackFace,
             unknown1: unknown1,
+            frontMaterialIndex: frontMaterialIndex,
+            backMaterialIndex: backMaterialIndex,
             vertexIndex: vertexIndex,
             texcoordIndex: texcoordIndex,
             unknown2: unknown2
@@ -55,7 +61,10 @@ public struct Polygon {
     public let index: Int
     public let count: Int
     public let type: Int
-    public let unknown1: Array<UInt16>
+    public let hasBackFace: UInt16
+    public let unknown1: UInt16
+    public let frontMaterialIndex: Int
+    public let backMaterialIndex: Int
     public let vertexIndex: Array<Int>
     public let texcoordIndex: Array<Int>
     public let unknown2: Array<UInt8>
