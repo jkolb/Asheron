60c8e43f6b425269f794f8c019544307d65095a7
Justin Kolb <franticapparatus@gmail.com>
2017-03-11 08:35:42 -0600

    Add LICENSE and README.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Asheron
+#### Asheron's Call data file parser
+
+## Contact
+
+[Justin Kolb](mailto:franticapparatus@gmail.com)  
+[@nabobnick](https://twitter.com/nabobnick)
+
+## License
+
+Asheron is available under the MIT license. See the LICENSE file for more info.
--- Tests/AsheronTests/AsheronTests.swift
+++ Tests/AsheronTests/AsheronTests.swift
@@ -1,3 +1,27 @@
+/*
+ The MIT License (MIT)
+ 
+ Copyright (c) 2017 Justin Kolb
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 import XCTest
 @testable import Asheron
 
@@ -32,13 +56,12 @@ class AsheronTests: XCTestCase {
         cStringBuffer.reset()
         XCTAssertEqual(cStringBuffer.getCString(), "ABC")
         
-        let indexFile = try! IndexFile.openForReading(at: "/Users/jkolb/src/Dereth/Data/client_portal.dat")
-        let textureHandles = try! indexFile.handles(matching: { UInt16($0 >> 16) == PortalKind.texture.rawValue })
-        print(textureHandles)
-        let portalFile = PortalFile(indexFile: indexFile)
-        let texture = try! portalFile.fetchTexture(handle: PortalHandle(rawValue: textureHandles.first!)!)
-        print(texture)
-//        XCTAssertEqual(Asheron().text, "Hello, World!")
+//        let indexFile = try! IndexFile.openForReading(at: "/Users/jkolb/src/Dereth/Data/client_portal.dat")
+//        let textureHandles = try! indexFile.handles(matching: { UInt16($0 >> 16) == PortalKind.texture.rawValue })
+//        print(textureHandles)
+//        let portalFile = PortalFile(indexFile: indexFile)
+//        let texture = try! portalFile.fetchTexture(handle: PortalHandle(rawValue: textureHandles.first!)!)
+//        print(texture)
     }
 
 
